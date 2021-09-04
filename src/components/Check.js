c9064024d850de012c6b4c9e85c43e46797be45d
ccortespaez <cristobal.cortespaez@gmail.com>
2021-09-04 12:30:19 -0400

    Arreglo en columnas

--- src/components/Check.js
+++ src/components/Check.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Check = ({check, deleteCheck}) => (  
-        <div className="col-6">
+        <div className="col-lg col-md-5">
         <div className="card p-4 mt-2">
-        <p className="text-white">Mascota:<span>{check.pet}</span> </p>
-        <p className="text-white">Dueño:<span>{check.owner}</span> </p>
-        <p className="text-white">Fecha:<span>{check.date}</span> </p>
-        <p className="text-white">Hora:<span>{check.time}</span> </p>
-        <p className="text-white">Sintomas:<span>{check.symptoms}</span> </p>
+        <p className="text-white">Mascota:<span> {check.pet}</span> </p>
+        <p className="text-white">Dueño:<span> {check.owner}</span> </p>
+        <p className="text-white">Fecha:<span> {check.date}</span> </p>
+        <p className="text-white">Hora:<span> {check.time}</span> </p>
+        <p className="text-white">Sintomas:<span> {check.symptoms}</span> </p>
         <button className="btn btn-danger" onClick={() => deleteCheck(check.id)}>Eliminar</button>
     </div>
     </div>
