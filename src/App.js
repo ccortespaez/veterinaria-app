1382528f712477a3aeccde2bce8dde5f6f2180f2
ccortespaez <cristobal.cortespaez@gmail.com>
2021-09-04 09:33:11 -0400

    Componente Form y estilos con bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+    <h1 className="text-center mt-2 mb-2 text-white fw-bold">Veterinaria App</h1>
+    <div className="row">
+      <div className="col-lg-5">
+        <Form/>
+      </div>
+      <div className="col-lg">
+        Mensaje de prueba
+      </div>
+
     </div>
+    </div>
+    </>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,50 @@
+import React, { Fragment } from "react";
+
+const Form = () => {
+  return (
+    <>
+      <h3 className="fw-bold text-white mt-3 text-center">Crear Cita</h3>
+      <div className="card">
+        <div className="card-body">
+          <form>
+            <div className="row">
+              <div className="col-lg-6">
+                <div className="form-group">
+                  <label className="mt-2">Nombre Mascota</label>
+                  <input className="form-control" />
+                </div>
+              </div>
+              <div className="col-lg">
+                <div className="form-group">
+                  <label className="mt-2">Nombre Dueño</label>
+                  <input className="form-control" />
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-6">
+                <div className="form-group">
+                  <label className="mt-2">Fecha</label>
+                  <input className="form-control" type="date" />
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="form-group">
+                  <label className="mt-2">Hora</label>
+                  <input className="form-control" type="time" />
+                </div>
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label className="mt-2">Nombre Dueño</label>
+              <textarea className="form-control mb-2"></textarea>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Form;
