d8deb39c0a4b7997ec6d0910c5fd58c6ec9f1c80
ccortespaez <cristobal.cortespaez@gmail.com>
2021-09-04 11:05:39 -0400

    Crear citas y mostrarlas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Form from './components/Form';
+import Check from './components/Check';
 
 function App() {
+
+  const [checks, setChecks] = useState([]);
+
+  const createCheck = check => {
+    setChecks([...checks,check])
+  }
+
   return (
     <>
     <div className="container">
     <h1 className="text-center mt-2 mb-2 text-white fw-bold">Veterinaria App</h1>
     <div className="row">
       <div className="col-lg-5">
-        <Form/>
+        <Form 
+        createCheck={createCheck}/>
       </div>
-      <div className="col-lg">
-        Mensaje de prueba
+      <div className="col-lg mt-2">
+        <h2 className="text-white fw-bold">Administra tus citas</h2>
+        {checks.map(check => (
+          <Check
+            key={check.id}
+            check={check}
+          />
+        ))}
       </div>
 
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,41 +1,90 @@
 import React, { Fragment, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const Form = () => {
+const Form = ({ createCheck }) => {
   //Crear state de citas
-    const [check, setCheck] = useState({
-        pet: '',
-        owner: '',
-        date: '',
-        time: '',
-        symptoms: ''
-    });
+  const [check, setCheck] = useState({
+    pet: "",
+    owner: "",
+    date: "",
+    time: "",
+    symptoms: "",
+  });
 
-    const handleChange = e => {
-        setCheck({
-            ...check,
-            [e.target.name] : e.target.value
-        })
+  const [error, setError] = useState(false);
+
+  const handleChange = (e) => {
+    setCheck({
+      ...check,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //Destructuring de la cita
+  const { pet, owner, date, time, symptoms } = check;
+
+  const submitCheck = (e) => {
+    e.preventDefault();
+
+    //Validacion
+    if (
+      pet.trim() === "" ||
+      owner.trim() === "" ||
+      date.trim() === "" ||
+      time.trim() === "" ||
+      symptoms.trim() === ""
+    ) {
+      setError(true);
+      return;
     }
 
-    const [pet, owner, date, time, symptoms] = check;
+    setError(false);
+
+    check.id = uuidv4();
+
+    createCheck(check);
+
+    setCheck({
+      pet: "",
+      owner: "",
+      date: "",
+      time: "",
+      symptoms: "",
+    });
+  };
 
   return (
     <>
       <h3 className="fw-bold text-white mt-3 text-center">Crear Cita</h3>
+      {error ? (
+        <p className="mt-2 alert alert-danger ">
+          Todos los campos son obligatorios{" "}
+        </p>
+      ) : null}
       <div className="card">
         <div className="card-body">
-          <form>
+          <form onSubmit={submitCheck}>
             <div className="row">
               <div className="col-lg-6">
                 <div className="form-group">
                   <label className="mt-2">Nombre Mascota</label>
-                  <input className="form-control" name="pet" onChange={handleChange} value={pet}/>
+                  <input
+                    className="form-control"
+                    name="pet"
+                    onChange={handleChange}
+                    value={pet}
+                  />
                 </div>
               </div>
               <div className="col-lg">
                 <div className="form-group">
                   <label className="mt-2">Nombre Dueño</label>
-                  <input className="form-control" name="owner" onChange={handleChange} value={owner}/>
+                  <input
+                    className="form-control"
+                    name="owner"
+                    onChange={handleChange}
+                    value={owner}
+                  />
                 </div>
               </div>
             </div>
@@ -43,13 +92,25 @@ const Form = () => {
               <div className="col-lg-6">
                 <div className="form-group">
                   <label className="mt-2">Fecha</label>
-                  <input className="form-control" type="date" name="date" onChange={handleChange} value={date}/>
+                  <input
+                    className="form-control"
+                    type="date"
+                    name="date"
+                    onChange={handleChange}
+                    value={date}
+                  />
                 </div>
               </div>
               <div className="col-lg-6">
                 <div className="form-group">
                   <label className="mt-2">Hora</label>
-                  <input className="form-control" type="time" name="time" onChange={handleChange} value={time}/>
+                  <input
+                    className="form-control"
+                    type="time"
+                    name="time"
+                    onChange={handleChange}
+                    value={time}
+                  />
                 </div>
               </div>
             </div>
@@ -57,10 +118,14 @@ const Form = () => {
               <label className="mt-2">Sintomas</label>
               <textarea
                 className="form-control mb-2"
-                name="symptoms" onChange={handleChange} value={symptoms}
+                name="symptoms"
+                onChange={handleChange}
+                value={symptoms}
               ></textarea>
             </div>
-            <button className="btn mx-auto d-grid gap-2 col-6">Ingresar</button>
+            <button className="btn mx-auto d-grid gap-2 col-6" type="submit">
+              Ingresar
+            </button>
           </form>
         </div>
       </div>
